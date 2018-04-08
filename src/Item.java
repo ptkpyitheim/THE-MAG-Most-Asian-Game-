d86138d38531e462a7341dbb7259757642e6d35d
pyitheimkyaw <pyitheimkyaw@10.61.4.116>
2018-04-08 15:16:28 -0400

    Added constructor, getters, setters, toString() method

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,41 @@
 
 public class Item {
-
+	private String name;
+	private String type;
+	private String description;
+	
+	public Item(String pName, String pType, String pDescription) {
+		name = pName;
+		type = pType;
+		description = pDescription;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setName(String pName) {
+		name = pName;
+	}
+	public void setType(String pType) {
+		type = pType;
+	}
+	public void setDescription(String pDescription) {
+		description = pDescription;
+	}
+	
+	public String toString() {
+		String ret = " ";
+		ret += "Item: \n";
+		ret += "      Name: " + name + "\n";
+		ret += "      Type: " + type + "\n";
+		ret += "      Description: " + description;
+		return ret;
+	}
+	
 }
